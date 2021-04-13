d35823ddb51e76853d79398f69ae4033fba216a6
gersonsinga <79317632+gersonsinga@users.noreply.github.com>
2021-04-13 16:10:27 +0200

    Try2

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -15,9 +15,7 @@ systemctl start docker
 usermod -aG docker ubuntu
 
 # run jenkins
-mkdir -p /var/jenkins_home
-chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins ubimoz/jenkins:latest
+sudo docker run -d -p8080:8080 -v /var/run/docker.sock:/var/run/docker.sock ubimoz/jenkins
 
 # show endpoint
 echo 'Jenkins installed'
