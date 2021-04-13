f5afc89043c057a863f113359682e639dbf51572
gersonsinga <79317632+gersonsinga@users.noreply.github.com>
2021-04-13 19:10:35 +0200

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# this script is only tested on ubuntu xenial
-# install docker
 
+# this script is only tested on ubuntu xenial
+
+# install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -11,7 +12,8 @@ apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
-usermod -aG docker jenkins
+usermod -aG docker ubuntu
+
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
@@ -20,4 +22,3 @@ docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
-
